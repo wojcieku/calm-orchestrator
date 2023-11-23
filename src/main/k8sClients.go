48da9d70abefa44a09e4b548da33319f0fdfab91
Jakub <wojcieku@gmail.com>
2023-11-23 13:11:45 +0100

    code cleanup

--- src/main/k8sClients.go
+++ src/main/k8sClients.go
@@ -1,1 +1,37 @@
 package main
+
+import (
+	log "github.com/sirupsen/logrus"
+	"k8s.io/client-go/dynamic"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
+	"os"
+	"path/filepath"
+)
+
+func getDynamicClientWithContextName(contextName string) dynamic.Interface {
+	userHomeDir, err := os.UserHomeDir()
+	if err != nil {
+		log.Panicf("error getting user home dir: %v\n", err)
+	}
+	kubeConfigPath := filepath.Join(userHomeDir, ".kube", "config")
+	serverKubeConfig, err := buildConfigWithContextFromFlags(contextName, kubeConfigPath)
+
+	if err != nil {
+		log.Panicf("Failed to create k8s API client from context name: %v\n", err)
+	}
+
+	dynClient, err := dynamic.NewForConfig(serverKubeConfig)
+	if err != nil {
+		log.Panicf("error creating dynamic client: %v\n", err)
+	}
+	return dynClient
+}
+
+func buildConfigWithContextFromFlags(context string, kubeconfigPath string) (*rest.Config, error) {
+	return clientcmd.NewNonInteractiveDeferredLoadingClientConfig(
+		&clientcmd.ClientConfigLoadingRules{ExplicitPath: kubeconfigPath},
+		&clientcmd.ConfigOverrides{
+			CurrentContext: context,
+		}).ClientConfig()
+}
--- src/main/main.go
+++ src/main/main.go
@@ -11,13 +11,10 @@ import (
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/client-go/dynamic"
-	"k8s.io/client-go/rest"
-	"k8s.io/client-go/tools/clientcmd"
-	"os"
-	"path/filepath"
 )
 
 // TODO konfiguracja jako parametr wywolania albo cos + parametr kubeconfiga
+// TODO debug mode - wstrzymuje usuniecie zasobow w przypadku bledu
 const CONFIG_PATH = "sampleConfig.yaml"
 
 func main() {
@@ -65,6 +62,8 @@ serverStatusLoop:
 			}
 		case commons.FAILURE:
 			{
+				fmt.Println("Resources will be deleted, check logs or press any key to proceed")
+				_, _ = fmt.Scanln()
 				deleteLatencyMeasurement(serverSideClient, serverSideLm)
 				log.Panic("Servers setup failed, deleting LatencyMeasurement in server side cluster..")
 			}
@@ -86,14 +85,13 @@ clientStatusLoop:
 			}
 		case commons.FAILURE:
 			{
+				fmt.Println("Resources will be deleted, check logs or press any key to proceed")
+				_, _ = fmt.Scanln()
 				deleteLatencyMeasurementsInBothClusters(serverSideClient, serverSideLm, clientSideClient, clientSideLm)
 				log.Panic("Clients setup failed, deleting LatencyMeasurements in both clusters..")
 			}
 		}
 	}
-	fmt.Println("input text:")
-	var w1, w2, w3 string
-	_, _ = fmt.Scanln(&w1, &w2, &w3)
 
 	// delete Client and Server side LMs
 	deleteLatencyMeasurementsInBothClusters(serverSideClient, serverSideLm, clientSideClient, clientSideLm)
@@ -126,31 +124,4 @@ func deleteLatencyMeasurement(client dynamic.Interface, lm commons.LatencyMeasur
 	if err != nil {
 		log.Errorf("LatencyMeasurement deletion in %s side cluster failed", lm.Spec.Side)
 	}
-}
-
-func getDynamicClientWithContextName(contextName string) dynamic.Interface {
-	userHomeDir, err := os.UserHomeDir()
-	if err != nil {
-		log.Panicf("error getting user home dir: %v\n", err)
-	}
-	kubeConfigPath := filepath.Join(userHomeDir, ".kube", "config")
-	serverKubeConfig, err := buildConfigWithContextFromFlags(contextName, kubeConfigPath)
-
-	if err != nil {
-		log.Panicf("Failed to create k8s API client from context name: %v\n", err)
-	}
-
-	dynClient, err := dynamic.NewForConfig(serverKubeConfig)
-	if err != nil {
-		log.Panicf("error creating dynamic client: %v\n", err)
-	}
-	return dynClient
-}
-
-func buildConfigWithContextFromFlags(context string, kubeconfigPath string) (*rest.Config, error) {
-	return clientcmd.NewNonInteractiveDeferredLoadingClientConfig(
-		&clientcmd.ClientConfigLoadingRules{ExplicitPath: kubeconfigPath},
-		&clientcmd.ConfigOverrides{
-			CurrentContext: context,
-		}).ClientConfig()
 }
