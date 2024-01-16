597d58dbde4ed6e88464f2f04a006aa612f9e601
Jakub <wojcieku@gmail.com>
2024-01-16 16:53:27 +0100

    main cleanup

--- src/main.go
+++ src/main.go
@@ -7,21 +7,7 @@ import (
 // TODO debug mode - wstrzymuje usuniecie zasobow w przypadku bledu
 
 func main() {
-	//configPath := flag.String("path", "", "Path to measurement configuration file")
-	//flag.Parse()
-
 	configMapController := controllers.NewCalmConfigMapController(controllers.GetClientSet())
 	configMapController.Run()
 	defer configMapController.Stop()
-
-	// load/read config
-	//configHandler := utils.MeasurementConfigHandler{}
-	//config := configHandler.LoadConfigurationFromPath(*configPath)
-
-	// TODO config bedzie z ConfigMapy - bedzie controller który przy każdym Create robi go measure(config)
-	// controller musi mieć listę kanałów ? chyba nie musi
-
-	// TODO ogarniecie sciezki do pliku z tokenami, to nizej zostaje do measure
-
-	// go measure(config):
 }
